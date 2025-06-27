47e80f12aaafd9eade877722800c33a84a853ca7
Henio Junior <henioajunior@gmail.com>
2025-06-27 08:28:01 -0300

    Regex - Limpar CPF

--- strings/csharp/demo-regex/Program.cs
+++ strings/csharp/demo-regex/Program.cs
@@ -6,9 +6,13 @@ namespace demo_regex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(ValidateCEP("12345-678")); // true
-            Console.WriteLine(ValidateCEP("12345678"));  // true
-            Console.WriteLine(ValidateCEP("1234-5678"));
+            //Console.WriteLine(ValidateCEP("12345-678")); // true
+            //Console.WriteLine(ValidateCEP("12345678"));  // true
+            //Console.WriteLine(ValidateCEP("1234-5678"));
+
+            Console.WriteLine(RemoveNonDigits("94923784799"));       // 94923784799
+            Console.WriteLine(RemoveNonDigits("213.445.034-82"));    // 21344503482
+
         }
 
         static bool ValidateCEP(string cep)
@@ -19,5 +23,12 @@ namespace demo_regex
 
         //Usamos System.Text.RegularExpressions.Regex para trabalhar com expressões regulares.
         //A regex ^\d{5}-?\d{3}$ continua a mesma: permite CEP com ou sem hífen (ex: "12345-678" ou "12345678").
+
+
+        static string RemoveNonDigits(string input)
+        {
+            Regex regex = new Regex(@"\D"); // \D corresponde a qualquer caractere que não seja dígito
+            return regex.Replace(input, "");
+        }
     }
 }

02c881a41062714a659df59bdfa07041e2a262c3
Henio Junior <henioajunior@gmail.com>
2025-06-27 15:38:35 -0300

    Regex - problema dominio-email

---
+++ strings/csharp/exercicios/EmailInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicios
+{
+    internal class EmailInfo
+    {
+        public string Usuario { get; set; }
+        public string Dominio { get; set; }
+        public string Brasileiro { get; set; }
+    
+    public EmailInfo(string usuario, string dominio)
+        {
+            Usuario = usuario;
+            Dominio = dominio;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Usuario: " + Usuario + "\n");
+            sb.Append("Domínio: " + Dominio + "\n");
+            sb.Append("Brasileiro: " + Brasileiro);
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ strings/csharp/exercicios/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+
+namespace exercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(RemoveNonDigits("87409217293"));
+            //Console.WriteLine(RemoveNonDigits("874092172-93"));
+            //Console.WriteLine(RemoveNonDigits("874.092.172-93"));
+
+            Console.WriteLine(ExtractEmailInformation("joao.silva23@yahoo.com.br"));
+            Console.WriteLine();
+            Console.WriteLine(ExtractEmailInformation("maria123@gmail.com"));
+
+        }
+
+        static string RemoveNonDigits(string str)
+        {
+            Regex regex = new Regex(@"\D");
+            return regex.Replace(str, "");
+        }
+
+        public static EmailInfo ExtractEmailInformation(string email)
+        {
+            string pattern = @"^(?<usuario>[^@]+)@(?<dominio>[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$";
+            Match match = Regex.Match(email,pattern);
+            EmailInfo emailInfo = null;
+
+            if (match.Success)
+            {
+                emailInfo = new EmailInfo(
+                    match.Groups["usuario"].Value,
+                    match.Groups["dominio"].Value
+                );
+                if (emailInfo.Dominio.EndsWith(".br"))
+                {
+                emailInfo.Brasileiro = "Sim";
+                }
+                else
+                {
+                    emailInfo.Brasileiro = "Não";
+
+                }
+
+
+            }
+            return emailInfo ?? new EmailInfo("Usuario não encontrado", "Dominio não encontrado");
+
+        }
+
+
+    }
+}
